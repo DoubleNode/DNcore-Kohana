8da6688cbe3b257d38c0384aa478055a2c9451e1
Jaye Miller <me@jayemiller.com>
2016-07-15 22:22:36 -0500

    Updates to call custom /bootstrap/bootstrap.php

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -1,5 +1,12 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+//
+if(file_exists('/bootstrap/bootstrap.php'))
+{
+	require_once('/bootstrap/bootstrap.php');
+	return;
+}
+
 // -- Environment setup --------------------------------------------------------
 
 // Load the core Kohana class
@@ -125,19 +132,10 @@ Kohana::$log->attach(new Log_File(APPPATH.'logs'));
 Kohana::$config->attach(new Config_File);
 
 /**
- * Enable modules. Modules are referenced by a relative or absolute path.
- */
-Kohana::modules(array(
-	// 'auth'       => MODPATH.'auth',       // Basic authentication
-	// 'cache'      => MODPATH.'cache',      // Caching with multiple backends
-	// 'codebench'  => MODPATH.'codebench',  // Benchmarking tool
-	// 'database'   => MODPATH.'database',   // Database access
-	// 'image'      => MODPATH.'image',      // Image manipulation
-	// 'minion'     => MODPATH.'minion',     // CLI Tasks
-	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
-	// 'unittest'   => MODPATH.'unittest',   // Unit testing
-	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-	));
+* Autoload composer libraries
+*
+*/
+require $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
 
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
